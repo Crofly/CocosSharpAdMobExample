0b7676fc1eabccd37200ab846746bbe64c992a1e
aschwarz <andreas.schwarz@elovent.de>
2015-09-28 12:00:01 +0200

    Create Test Project

---
+++ CCAdmobTest/GameAppDelegate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CocosSharp;
+using Android.App;
+
+
+namespace CCAdmobTest
+{
+	public class GameAppDelegate : CCApplicationDelegate
+	{
+		// the activity which launched the game
+		private Activity activity;
+
+		public GameAppDelegate(Activity openerActivity)
+		{
+			this.activity = openerActivity;
+		}
+
+		public override void ApplicationDidFinishLaunching (CCApplication application, CCWindow mainWindow)
+		{
+			application.PreferMultiSampling = false;
+			application.ContentRootDirectory = "Content";
+			application.ContentSearchPaths.Add ("animations");
+			application.ContentSearchPaths.Add ("fonts");
+			application.ContentSearchPaths.Add ("sounds");
+
+			CCSize windowSize = mainWindow.WindowSizeInPixels;
+
+			float desiredWidth = 1024.0f;
+			float desiredHeight = 768.0f;
+            
+			// This will set the world bounds to be (0,0, w, h)
+			// CCSceneResolutionPolicy.ShowAll will ensure that the aspect ratio is preserved
+			CCScene.SetDefaultDesignResolution (desiredWidth, desiredHeight, CCSceneResolutionPolicy.ShowAll);
+            
+			// Determine whether to use the high or low def versions of our images
+			// Make sure the default texel to content size ratio is set correctly
+			// Of course you're free to have a finer set of image resolutions e.g (ld, hd, super-hd)
+			if (desiredWidth < windowSize.Width) {
+				application.ContentSearchPaths.Add ("images/hd");
+				CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
+			} else {
+				application.ContentSearchPaths.Add ("images/ld");
+				CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
+			}
+            
+			CCScene scene = new CCScene (mainWindow);
+			GameLayer gameLayer = new GameLayer (activity);
+
+			scene.AddChild (gameLayer);
+
+			mainWindow.RunWithScene (scene);
+		}
+
+		public override void ApplicationDidEnterBackground (CCApplication application)
+		{
+			application.Paused = true;
+		}
+
+		public override void ApplicationWillEnterForeground (CCApplication application)
+		{
+			application.Paused = false;
+		}
+	}
+}
---
+++ CCAdmobTest/GameLayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using CocosSharp;
+using Android.App;
+using Android.Views;
+using Android.Gms.Ads;
+
+namespace CCAdmobTest
+{
+	public class GameLayer : CCLayer
+	{
+		private Activity activity;
+
+		private float playTime = 0.0f;
+
+		public GameLayer (Activity activity)
+		{
+			this.activity = activity;
+		}
+
+		protected override void AddedToScene ()
+		{
+			base.AddedToScene ();
+
+			// Use the bounds to layout the positioning of our drawable assets
+			CCRect bounds = VisibleBoundsWorldspace;
+
+			// Register for touch events
+			var touchListener = new CCEventListenerTouchAllAtOnce ();
+			touchListener.OnTouchesEnded = OnTouchesEnded;
+			AddEventListener (touchListener, this);
+
+
+			Android.Graphics.Point size = new Android.Graphics.Point();
+			activity.WindowManager.DefaultDisplay.GetSize(size);
+			ViewGroup.LayoutParams adParams = new ViewGroup.LayoutParams(
+				size.X,                        
+				100);
+
+			var ad = new AdView (activity);
+			ad.AdSize = AdSize.SmartBanner;
+			ad.AdUnitId = "YOUR ADD ID HERE";
+			ad.LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent);
+			var requestbuilder = new AdRequest.Builder ();
+			ad.LoadAd (requestbuilder.Build ());
+
+			activity.AddContentView(ad,adParams);
+			ad.BringToFront ();
+
+			Schedule (t => {
+				playTime += t;
+				if (playTime > 3 ) {
+					ad.BringToFront();
+				}
+			}, 1.0f);
+
+		}
+
+		void OnTouchesEnded (List<CCTouch> touches, CCEvent touchEvent)
+		{
+			if (touches.Count > 0) {
+				// Perform touch handling here
+			}
+		}
+	}
+}
